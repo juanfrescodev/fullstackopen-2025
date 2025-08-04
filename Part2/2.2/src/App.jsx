7c329f2cd555474097822b916bf5ba5ae73c0122
Juan Fresco <juanfresco1@gmail.com>
2025-08-04 18:57:12 -0300

    Add files via upload

---
+++ Part2/2.2/src/App.jsx
@@ -0,0 +1,79 @@
+const Header = ({course}) => {
+  return (
+      <div>
+          <h1>{course.name}</h1>
+      </div>
+    )
+  }
+
+
+
+const Part = ({part}) =>{
+  return(
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({course}) =>{
+  return(
+    <div>
+      {course.parts.map(part =>(
+        <Part key = {part.id} part={part} />      
+      ))}
+    </div>
+  )
+}
+
+const Total = ({course}) => {
+  const total = course.parts.reduce((sum,part)=> sum + part.exercises, 0)
+  return(
+    <p><strong>Total of {total} exercises</strong></p>
+  )
+}
+
+const Course = ({course}) =>{
+  return (
+    <div>
+    <Header course = {course}  />
+    <Content course = {course} />
+    <Total course={course} />
+    </div>
+  )
+  //aca deberia poner algo que reciba las props de course y lo mande a const header?
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+      {
+        name: 'Redux',
+        exercises: 11,
+        id: 4
+        
+      }
+    ]
+  }
+
+  return <Course course={course} />
+}
+
+export default App
