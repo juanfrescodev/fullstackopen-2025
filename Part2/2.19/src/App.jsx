c56d5bf8ea8068bfa2c4257c4aee176d47757032
Juan Fresco <juanfresco1@gmail.com>
2025-08-19 14:03:11 -0300

    Add files via upload

---
+++ Part2/2.19/src/App.jsx
@@ -0,0 +1,92 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const PaisSeleccionado = ({pais}) =>{
+  return(
+    <div>
+      <h1>{pais.name.common}</h1>
+      <p>Capital {pais.capital}</p>
+      <p>Area {pais.area}</p>
+      <h2>Languages</h2>
+      <ul>
+        {Object.values(pais.languages).map((lang, id)=>(
+          <li key = {id}> {lang}</li>
+          ))}
+      </ul>
+      <img src ={pais.flags.png} alt = {pais.flags.alt} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [value, setValue] = useState('')
+  const [paises, setPaises] = useState([])
+  const [filteredPaises, setFilteredPaises] = useState([])
+  const [selected, setSelected] = useState(null)
+
+  useEffect(() => {
+    axios
+      .get(`https://studies.cs.helsinki.fi/restcountries/api/all`)
+      .then(response => {
+        setPaises(response.data)
+        setFilteredPaises(response.data)
+      })
+  }, [])
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  useEffect(() => {
+    const filtered = paises.filter(pais => 
+      pais.name.common.toLowerCase().includes(value.toLowerCase())
+    )
+    setFilteredPaises(filtered)
+    if (filtered.length > 1){
+      setSelected(null)
+    }  
+    if (filtered.length === 1){
+      setSelected(filtered[0])
+    }
+  }, [value,paises])
+
+
+  const lista = () =>{
+    const len = filteredPaises.length
+
+    if (selected !== null){
+      return <PaisSeleccionado pais={selected}/>
+    }
+    
+    if (value === ''){
+      return <p>Type a country to search</p>
+    }
+
+    if (len > 10) {
+      return <p>Too many matches, specify another filter</p>
+    }
+
+    if (len > 1) {
+      return(
+        <ul>
+         {filteredPaises.map((pais,index) => (
+          <li key = {index}> {pais.name.common}
+          <button onClick={() => setSelected(pais)}>Show</button></li>
+        ))
+        }
+      </ul> )
+    }
+    return <p>No country matches the search</p>    
+  }
+
+  return (
+    <div>
+      <form>
+        find countries: <input value={value} onChange={handleChange} />
+      </form>
+      {lista()}
+    </div>
+  )
+}
+
+export default App
